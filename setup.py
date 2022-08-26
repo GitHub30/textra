b8f32d1a017de7880bcfcf7bc07ea5bfe9b7536e
言葉 <funaox@gmail.com>
2022-08-27 03:04:03 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+setup(
+    name='textra',
+    version='0.1',
+    description='Machine translation for everyone',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/GitHub30/textra',
+    project_urls={ 'Bug Tracker': 'https://github.com/GitHub30/textra/issues' },
+    author='TexTra',
+    author_email='funaox@gmail.com',
+    platforms="any",
+    license='MIT',
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Utilities",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords="TexTra translation python",
+    install_requires=['requests', 'requests-oauthlib'],
+    py_modules=['textra'],
+    entry_points={
+        'console_scripts': [
+            'trans = textra:trans'
+        ]
+    }
+)
+
+# Publish commands
+# https://packaging.python.org/tutorials/packaging-projects/
+#pip install --upgrade pip build twine
+#python3 -m build
+#python3 -m twine upload dist/*
